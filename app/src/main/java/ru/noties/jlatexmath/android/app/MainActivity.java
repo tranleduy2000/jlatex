69f4c1da276d43023a6f024660c1baec113c08b5
duy <tranleduy1233@gmail.com>
2020-05-31 16:28:51 +0700

    at (@) symbol

--- app/src/main/java/ru/noties/jlatexmath/android/app/MainActivity.java
+++ app/src/main/java/ru/noties/jlatexmath/android/app/MainActivity.java
@@ -115,6 +115,7 @@ public class MainActivity extends Activity {
         put("latex #6", LATEX_6);
         put("latex #7", LATEX_7);
         put("latex #8", "{\\textnormal{abcdefff}}_{16}");
+        put("latex #9", "InverseErfc\\text{/@}\\{0,1,2\\}");
     }};
     private JLatexMathView jLatexMathView;
 
